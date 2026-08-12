8d8c1e4590c4b48731d8bce4d3b94cb201834e4e
0x0m-RAM <niboroxom@gmail.com>
2026-08-12 04:53:26 -0400

    Weitere Dateien im Robin-Ordner hinzugefügt

---
+++ Robin/radius.js
@@ -0,0 +1,8 @@
+const radius = 5;
+const flaeche = Math.PI * radius ** 2;
+
+console.log("Kreisfläche:", flaeche);
+
+const zahl = 17
+const istGerade = zahl % 2 === 0;
+console.log("ist 17 gerade?", istGerade);
---
+++ Robin/script.js
@@ -0,0 +1,9 @@
+const zahl = 42;
+const text = "Hallo JavaScript";
+const istAktiv = true;
+const farben = ["Rot", "Grün", "Blau"];
+
+console.log(typeof zahl);
+console.log(typeof text);
+console.log(typeof istAktiv);
+console.log(typeof farben);
